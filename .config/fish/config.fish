c542746dcb91b5dc178a2b1664ed3a82672f3764
Yong Bakos <ybakos@humanoriented.com>
2019-11-08 23:04:03 -0800

    Fish: Add config.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,7 @@
+set -gx PATH  /usr/local/sbin /usr/local/go/bin $HOME/projects/go/bin $PATH
+set PKG_CONFIG_PATH /usr/lib/pkgconfig /usr/lib64/pkgconfig /usr/local/lib64/pkgconfig /usr/local/lib/pkgconfig /usr/local/share/pkgconfig $PKG_CONFIG_PATH
+set LD_LIBRARY_PATH /usr/local/lib64/ /usr/local/lib/ $LD_LIBRARY_PATH
+set -x GOPATH $HOME/projects/go
+test -s /home/ybakos/.nvm-fish/nvm.fish; and source /home/ybakos/.nvm-fish/nvm.fish
 rvm default
+
